aef66dab1cf5a8766d594abda9a963e64b20d93c
Ilyas Sung <ilyassung@gmail.com>
2022-07-07 21:47:15 +0100

    Can add data to the firestore database

--- src/components/AddListControl.jsx
+++ src/components/AddListControl.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Stack, TextField, Divider } from '@mui/material';
+import { doc, setDoc } from 'firebase/firestore';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { firestore, auth } from '../lib/firebase';
 
 function AddListControl() {
+  const [user] = useAuthState(auth);
+  const [name, setName] = useState('');
+  const [quantity, setQuantity] = useState('');
+
+  const createGroceryList = () => {
+    const docRef = doc(firestore, "Users", user.uid);
+    const data = {
+      name: name,
+      quantity: quantity
+    }
+    return setDoc(docRef, data);
+  };
+
   return (
     <Stack
       direction="row"
@@ -9,15 +25,33 @@ function AddListControl() {
       alignItems="flex-end"
       spacing={2}
     >
-      <TextField id="standard-basic" label="Add" variant="standard" />
-      <TextField id="standard-basic" label="#" variant="standard" style = {{width: 50}} />
+      <TextField 
+        label="Add" 
+        variant="standard"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <TextField 
+        label="#" 
+        variant="standard" 
+        style={{width: 50}}
+        value={quantity} 
+        onChange={(e) => setQuantity(e.target.value)}
+      />
       <Stack
         direction="row"
         justifyContent="center"
         alignItems="flex-end"
         spacing={2}
       >
-        <Button variant="outlined">Add</Button>
+        <Button 
+          variant="outlined" 
+          onClick={() => {
+            createGroceryList("ilyassung@gmail.com");
+          } }
+        >
+          Add
+        </Button>
         <Divider orientation="vertical" flexItem />
         <Button variant="outlined">Clear List</Button>
       </Stack>
@@ -26,3 +60,4 @@ function AddListControl() {
 }
 
 export default AddListControl;
+
